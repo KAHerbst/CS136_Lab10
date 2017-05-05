699a8cd686c71d1721733c1a4852073cc4c53660
KAHerbst <kah3@williams.edu>
2017-05-05 09:33:02 -0400

    Update Human.java

--- Human.java
+++ Human.java
@@ -10,7 +10,14 @@ public class Human implements Player{
     // post: game is played from this node on; winning player is returned
 	//opponent can be the switch of Human color in our HexBoard class
     public Player play(GameTree node, Player opponent){
-	return null
-    }
+	    //as long as nobody has won at that node
+	if(!node.board.win(opponent.color) || !node.board.win(this.color))
+	  {
+		//look in the nodes children and print them out
+		//choose one via Scanner 
+		//set the new gametree with the root at that choice
+		//return a GameTree
+	    return opponent;
+	}	
 }
 
